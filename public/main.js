d0d038c014ead5f83bebf926a1486d84ae97e5b3
wangding <408542507@qq.com>
2021-06-25 17:05:03 +0800

    little

--- public/main.js
+++ public/main.js
@@ -1,10 +1,11 @@
 /* global fetch, location: true */
-$(() => {
-  let isLogin = localStorage.getItem('isLogin') || false;
+const q = document.querySelector,
+      $ = q.bind(document);
 
-  if(isLogin) {
-    $todoList.show();
-  } else {
-    $login.show();
-  }
-});
+let isLogin = localStorage.getItem('isLogin') || false;
+
+if(isLogin) {
+  $todoList.show();
+} else {
+  $login.show();
+}
